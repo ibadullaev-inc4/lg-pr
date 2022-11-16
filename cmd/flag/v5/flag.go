e3112587ae07e8756d190e70a94f891344514a05
Nariman Ibadullayev <n.ibadullaev@inc4.net>
2022-11-16 13:48:36 +0400

    flav v5

---
+++ cmd/flag/v5/flag.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/urfave/cli.v1"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "test cli app"
+	app.Description = "New cli application"
+	app.Usage = "New cli application"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "name,n",
+			Value: "No name",
+		},
+		cli.StringFlag{
+			Name:  "surname,s",
+			Value: "No Surname",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		name := c.String("name")
+		surname := c.String("surname")
+		fmt.Println(name, surname)
+		return nil
+	}
+
+	app.Run(os.Args)
+}
